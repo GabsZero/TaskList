6b8ae546e9fd459b969381f7efa76cf392f20cac
Joao Gabriel Souza <joaogbdesouza@gmail.com>
2019-05-15 21:40:02 -0300

    Starting project

---
+++ TasksList/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TasksList.Models
+{
+    public class Task
+    {
+        public int TaskId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string Weight { get; set; }
+    }
+}

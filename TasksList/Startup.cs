6f6b458c8b431965cde5c32fd785d26476e38a09
Joao Gabriel Souza <joaogbdesouza@gmail.com>
2019-05-15 22:11:30 -0300

    Adding controller with views

--- TasksList/Startup.cs
+++ TasksList/Startup.cs
@@ -59,7 +59,7 @@ namespace TasksList
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Assignments}/{action=Index}/{id?}");
             });
         }
     }
